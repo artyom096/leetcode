525b91213ec0a05335bb737dfeb01aa7e210ae68
artiomshurygin <artyom.shurygin@noorsoft.ru>
2023-04-04 22:18:07 +0300

    04.04.2023 array excirsises

---
+++ 4. squaresSortedArray.js
@@ -0,0 +1,19 @@
+const arr = [-4,-1,0,3,10]
+
+const sortedSquares = (nums) => {
+    const res = []
+    let end = nums.length - 1
+    let start = 0
+    let pos = end
+
+    while(start <= end) {
+        if(nums[start] ** 2 > nums[end] ** 2){
+            res[pos--] = nums[start++] ** 2
+           } else {
+            res[pos--] = nums[end--] ** 2
+        }
+    }
+    return res
+}
+
+console.log(sortedSquares(arr));

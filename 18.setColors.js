a8689aab4fb0d9e94897881fb6c545e41d8c1a6d
artiomshurygin <elekinshik@gmail.com>
2023-05-04 10:29:28 +0300

    added merge sort method

--- 18.setColors.js
+++ 18.setColors.js
@@ -40,23 +40,49 @@
 // }
 
 // quick
-var sortColors = function (nums) {
-  if (nums.length < 2) return nums;
+// var sortColors = function (nums) {
+//   if (nums.length < 2) return nums;
 
-  const center = nums[Math.floor(nums.length / 2)];
-  const less = [];
-  const more = [];
-  const equal = [];
+//   const center = nums[Math.floor(nums.length / 2)];
+//   const less = [];
+//   const more = [];
+//   const equal = [];
 
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] > center) {
-      more.push(nums[i]);
-    } else if (nums[i] < center) {
-      less.push(nums[i]);
-    } else {
-      equal.push(nums[i]);
-    }
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] > center) {
+//       more.push(nums[i]);
+//     } else if (nums[i] < center) {
+//       less.push(nums[i]);
+//     } else {
+//       equal.push(nums[i]);
+//     }
+//   }
+//   return [...sortColors(less), ...equal, ...sortColors(more)];
+// };
+
+// merge
+const mergeSort = (array) => {
+  const half = Math.floor(array.length / 2);
+  if (array.length < 2) {
+    return array;
   }
-  return [...sortColors(less), ...equal, ...sortColors(more)];
+
+  const left = array.slice(0, half);
+  const right = array.slice(half);
+  return merge(mergeSort(left), mergeSort(right));
 };
 
+const merge = (left, right) => {
+  const arr = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      arr.push(left.shift());
+    } else {
+      arr.push(right.shift());
+    }
+  }
+
+  return [...arr, ...left, ...right];
+};
+
+console.log(mergeSort([2, 5, 1, 7, 3, 2, 8, 4, 9]));

149d9f5f263186aa7a4b6c3b7023449459ed991c
artiomshurygin <elekinshik@gmail.com>
2023-07-17 07:59:02 +0300

    17.07.2023

---
+++ 07.23/12.ArrayPrototypeLats.js
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/array-prototype-last/description/
+
+Array.prototype.last = function () {
+  if (!this.length) return -1;
+  return this[this.length - 1];
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */

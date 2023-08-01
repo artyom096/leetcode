9d7eae31d1a3392386d343f194330e3f224b7cd8
artiomshurygin <elekinshik@gmail.com>
2023-08-01 08:09:21 +0300

    01.08.2023

---
+++ 07.23/21.dublicateArray.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+  const obj = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (obj[nums[i]]) {
+      return true;
+    }
+    obj[nums[i]] = true;
+  }
+  return false;
+};
